24e5826fe967943b4fe3dfcc1379b61cc8c41fc8
René Schwaiger <sanssecours@me.com>
2018-09-14 15:57:13 +0200

    Main: Print converted key set

--- Source/yawn.cpp
+++ Source/yawn.cpp
@@ -1,5 +1,6 @@
 // -- Imports ------------------------------------------------------------------
 
+#include <iostream>
 #include <stdlib.h>
 
 #include <kdb.hpp>
@@ -10,11 +11,26 @@ using ckdb::keyNew;
 using kdb::Key;
 using kdb::KeySet;
 
+using std::cout;
+using std::endl;
+
+// -- Functions ----------------------------------------------------------------
+
+void printOutput(KeySet const &keys) {
+  cout << endl << "— Output ————" << endl << endl;
+  for (auto key : keys) {
+    cout << key.getName() << ":"
+         << (key.getStringSize() > 1 ? " " + key.getString() : "") << endl;
+  }
+}
+
 // -- Main ---------------------------------------------------------------------
 
 int main() {
   KeySet keys;
   Key parent{keyNew("user", KEY_END, "", KEY_VALUE)};
 
-  return addToKeySet(keys, parent, "");
+  int status = addToKeySet(keys, parent, "");
+  printOutput(keys);
+  return (status >= 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
