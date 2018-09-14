eeaba565b0c06d1f79835c95bbaf29ac59596bde
René Schwaiger <sanssecours@me.com>
2018-09-14 16:58:26 +0200

    Memory: Split declaration and implementation

--- Source/memory.hpp
+++ Source/memory.hpp
@@ -1,33 +1,34 @@
 /**
  * @file
  *
- * @brief This file contains a wrapper for YAEP’s `objstack`.
+ * @brief This file contains declaration for a class used to allocate memory.
  *
- * About 50 percent of this code was copied from YAEP’s test suite.
- *
- * @copyright GPL License
- *            (see https://github.com/vnmakarov/yaep/blob/master/copyright)
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
  */
-
 #ifndef ELEKTRA_PLUGIN_YAWN_MEMORY_HPP
 #define ELEKTRA_PLUGIN_YAWN_MEMORY_HPP
 
+// -- Imports ------------------------------------------------------------------
+
 #include "objstack.h"
 
+// -- Class --------------------------------------------------------------------
+
+/** This class contains a wrapper for a stack of memory objects. */
 class Memory {
   /* All parser allocated memory is contained in this class. */
   os_t parseTreeMemory;
 
 public:
-  void *allocate(int size) {
-    void *result;
-
-    parseTreeMemory.top_expand(size);
-    result = parseTreeMemory.top_begin();
-    parseTreeMemory.top_finish();
-
-    return result;
-  }
+  /**
+   * This method allocates a memory region of the given size.
+   *
+   * @param size This variable specifies the amount of data this method should
+   *             allocate.
+   *
+   * @return A pointer to a memory region of the specified size
+   */
+  void *allocate(int size);
 };
 
 #endif // ELEKTRA_PLUGIN_YAWN_MEMORY_HPP
