5971108f525b8569538ff4a25c605b269f99a15c
René Schwaiger <sanssecours@me.com>
2018-09-19 13:20:34 +0200

    Location: Add initial version of class

---
+++ Source/location.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file
+ *
+ * @brief This file contains a class specifying the location of a token in a
+ *        text.
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+#ifndef ELEKTRA_PLUGIN_YAWN_LOCATION_HPP
+#define ELEKTRA_PLUGIN_YAWN_LOCATION_HPP
+
+// -- Imports ------------------------------------------------------------------
+
+#include "position.hpp"
+
+// -- Class --------------------------------------------------------------------
+
+/** This class represents the location of a token. */
+class Location {
+public:
+  /** This variable stores the start position of the token. */
+  Position begin;
+  /** This attribute stores the position where the token ends. */
+  Position end;
+};
+
+#endif // ELEKTRA_PLUGIN_YAWN_LOCATION_HPP
