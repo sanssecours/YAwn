db4d3541aba56318118ca02afd7e550a0a57895e
René Schwaiger <sanssecours@me.com>
2018-09-20 11:49:10 +0200

    Location: Add methods to update end position

--- Source/location.hpp
+++ Source/location.hpp
@@ -28,6 +28,27 @@ public:
    * @brief This method sets the start position to the current end position.
    */
   void step() { begin = end; }
+
+  /**
+   * @brief This method increases the column number of the end position by a
+   *        certain amount.
+   *
+   * @param offset This variable specifies how much the current column number
+   *               should be increased.
+   *
+   * @return A location that mirrors the old location number, except for the
+   *         column number of the end position, which was increased by the
+   *         amount `offset`.
+   */
+  Location &operator+=(size_t const offset) {
+    end.column += offset;
+    return *this;
+  }
+
+  /**
+   * @brief This method increases the line number of the end position by one.
+   */
+  void lines() { end.line++; }
 };
 
 #endif // ELEKTRA_PLUGIN_YAWN_LOCATION_HPP
