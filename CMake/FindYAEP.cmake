e722489b8f79f0770e6bab98e5cc3f0553deaf52
René Schwaiger <sanssecours@me.com>
2018-09-23 10:50:32 +0200

    Build: Find C and C++ YAEP libraries

--- CMake/FindYAEP.cmake
+++ CMake/FindYAEP.cmake
@@ -1,20 +1,19 @@
 # Try to find YAEP
 # ================
 
-# ~~~
-# Once done this will define
+# This find script defines the following variables:
 #
-#  YAEP_FOUND         System has YAEP
-#  YAEP_INCLUDE_DIRS  The YAEP include directories
-#  YAEP_LIBRARIES     The libraries needed to use YAEP
+# * YAEP_FOUND         System has YAEP
+# * YAEP_INCLUDE_DIRS  The YAEP include directories
+# * YAEP_LIBRARIES     The library needed to use YAEP (C)
+# * YAEP_LIBRARIES_CPP The library needed to use YAEP (C++)
 #
-# This script is a modified version of the code available here:
-# https://cmake.org/Wiki/CMake:How_To_Find_Libraries#Writing_find_modules
-# ~~~
+# .
 
 find_path(YAEP_INCLUDE_DIR NAMES yaep.h)
 
-find_library(YAEP_LIBRARY NAMES yaep++)
+find_library(YAEP_LIBRARY NAMES yaep)
+find_library(YAEP_LIBRARY_CPP NAMES yaep++)
 
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set YAEP_FOUND to TRUE, if all
@@ -22,9 +21,11 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(YAEP
                                   REQUIRED_VARS
                                   YAEP_LIBRARY
+                                  YAEP_LIBRARY_CPP
                                   YAEP_INCLUDE_DIR)
 
 mark_as_advanced(YAEP_INCLUDE_DIR YAEP_LIBRARY)
 
 set(YAEP_LIBRARIES ${YAEP_LIBRARY})
+set(YAEP_LIBRARIES_CPP ${YAEP_LIBRARY_CPP})
 set(YAEP_INCLUDE_DIRS ${YAEP_INCLUDE_DIR})
