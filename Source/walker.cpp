8422ccc224285522066f592529424343f69ffe31
René Schwaiger <sanssecours@me.com>
2018-09-23 09:47:47 +0200

    Walker: Add support for sequences

--- Source/walker.cpp
+++ Source/walker.cpp
@@ -64,6 +64,48 @@ string toString(yaep_tree_node const *const node, string const indent = "") {
 }
 
 /**
+ * @brief This function will be called before the walker enters an abstract
+ *        tree node.
+ *
+ * @param listener The function calls methods of this class when it encounters
+ *                 an abstract node with a certain name.
+ * @param node This argument stores the abstract tree node
+ */
+void executeEnter(Listener &listener, yaep_anode const &anode) {
+  if (string(anode.name) == "sequence") {
+    listener.enterSequence();
+  } else if (string(anode.name) == "element") {
+    listener.enterElement();
+  }
+}
+
+/**
+ * @brief This function will be called after the walker exits an abstract
+ *        tree node.
+ *
+ * @param listener The function calls methods of this class when it encountered
+ *                 an abstract node with a certain name.
+ * @param node This argument stores the abstract tree node
+ */
+void executeExit(Listener &listener, yaep_anode const &anode) {
+  if (string(anode.name) == "value") {
+    auto token = anode.children[0]->val.term.attr;
+    listener.exitValue((**static_cast<unique_ptr<Token> *>(token)).getText());
+  } else if (string(anode.name) == "key") {
+    auto token = anode.children[0]->val.term.attr;
+    listener.exitKey((**static_cast<unique_ptr<Token> *>(token)).getText());
+  } else if (string(anode.name) == "pair") {
+    bool matchedValue =
+        anode.children[1]->type != yaep_tree_node_type::YAEP_NIL;
+    listener.exitPair(matchedValue);
+  } else if (string(anode.name) == "sequence") {
+    listener.exitSequence();
+  } else if (string(anode.name) == "element") {
+    listener.exitElement();
+  }
+}
+
+/**
  * @brief This function traverses a tree executing methods of a listener class.
  *
  * @param listener The function calls methods of this class while it traverses
@@ -81,22 +123,14 @@ void executeListenerMethods(Listener &listener, yaep_tree_node const *node) {
   // Node is abstract
   yaep_anode anode = node->val.anode;
 
+  executeEnter(listener, anode);
+
   yaep_tree_node **children = anode.children;
   for (size_t child = 0; children[child]; child++) {
     executeListenerMethods(listener, children[child]);
   }
 
-  if (string(anode.name) == "value") {
-    auto token = anode.children[0]->val.term.attr;
-    listener.exitValue((**static_cast<unique_ptr<Token> *>(token)).getText());
-  } else if (string(anode.name) == "key") {
-    auto token = anode.children[0]->val.term.attr;
-    listener.exitKey((**static_cast<unique_ptr<Token> *>(token)).getText());
-  } else if (string(anode.name) == "pair") {
-    bool matchedValue =
-        anode.children[1]->type != yaep_tree_node_type::YAEP_NIL;
-    listener.exitPair(matchedValue);
-  }
+  executeExit(listener, anode);
 }
 
 } // namespace
